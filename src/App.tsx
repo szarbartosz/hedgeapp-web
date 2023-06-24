63b13fb0d96fd97c0b1cebdce0c202dd03dde569
Bartosz Szar <szarbartosz@gmail.com>
2023-06-25 01:02:27 +0200

    redirect from root route to locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-const App: React.FC = () => (
-  <div className="max-h-screen">
-    <Navbar />
-    <div className="flex min-w-max justify-center px-6 pb-6 pt-20 text-neutral-600 sm:px-8 sm:pb-8 lg:px-10 lg:pb-10">
-      <Outlet />
+const App: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/locations');
+  }, []);
+
+  return (
+    <div className="max-h-screen">
+      <Navbar />
+      <div className="flex min-w-max justify-center px-6 pb-6 pt-20 text-neutral-600 sm:px-8 sm:pb-8 lg:px-10 lg:pb-10">
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
